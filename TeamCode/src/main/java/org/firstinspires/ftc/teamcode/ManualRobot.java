2de0c25db947138024e88984f3c58744da0a4414
Edward Balasingham <ed@taniustech.com>
2024-10-02 22:03:00 -0700

    [slide] comment out slide subsystem since it isn't on the test robot it errors out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualRobot.java
@@ -16,7 +16,7 @@ public class ManualRobot {
     public ManualRobot(Config cfg) {
         config = cfg;
         subSystems.add(new Drive(config));
-        subSystems.add(new Slide(config));
+//        subSystems.add(new Slide(config));
     }
 
     //
